339074cf169fd663bdd3448384cf61df02ed7734
mauro287 <mr.naulaq2@uea.edu.ec>
2024-08-31 16:59:25 -0600

    deber de la semana 11 ordenacion de matriz

--- OPeraciones con arreglo/ejercicio 2.py
+++ OPeraciones con arreglo/ejercicio 2.py
@@ -4,3 +4,19 @@ matriz=[
     [3,6,10],
     [7,8,9]
 ]
+
+print(matriz)
+#blucle de ordenamiento blucle_sort
+def bubble_sort(fila):
+    # algoritmo de blucle sort ejemplo
+   n=len(fila)
+   for i in range(n):
+       for j in range (n-1,i,-1):
+           if fila[j]>fila[j-1]:
+               fila[j], fila[j-1]=fila[j-1], fila[j]
+               print(fila)
+
+print("matriz original")
+print(matriz)
+bubble_sort(matriz[1])
+print(matriz)
